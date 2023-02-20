4aed5c1c92defac1ae26d4d5f7a6467dda8ff209
Elaskuss <ivan.k@live.se>
2023-02-20 17:02:13 +0100

    Styling

--- src/components/directory-item/directory.item.styles.jsx
+++ src/components/directory-item/directory.item.styles.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const DirectoryContainer = styled.div`
    min-width: 30%;
-   height: 240px;
+   height: 40vh;
    flex: 1 1 auto;
    display: flex;
    align-items: center;
