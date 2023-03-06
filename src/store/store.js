2108d480c8a76de84ea42e171d95ac8a801dbaf7
Elaskuss <ivan.k@live.se>
2023-03-06 13:03:21 +0100

    middleware remove

--- src/store/store.js
+++ src/store/store.js
@@ -4,7 +4,7 @@ import { rootReducer } from "./root-reducer";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
-const middleWares = [logger]
+const middleWares = [process.env.NODE_ENV === "development" && logger].filter(Boolean);
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
 const persistConfig = {
